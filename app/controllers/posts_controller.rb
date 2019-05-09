4b766b003ee68f138f528cadcb44fc3452ed19ae
Peter <ruecktenwald@gmail.com>
2019-05-08 22:53:59 -0400

    add post actions - ability to create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show,:edit,:destroy]
+	
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save 
+			redirect_to @post, notice: "You successfully created a new post!"
+		else
+			render :new
+		end
+	end
+
+	def show	
+	end
+end
+
+
+private
+
+def post_params
+	params.require(:post).permit(:category,:description,:code)
+end
+
+def set_post
+	@post = Post.find(params[:id])
 end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -15,7 +15,26 @@ RSpec.describe Post, type: :model do
         visit posts_path
         expect(page).to have_content(/Posts/)
       end
+    end
+
+    feature 'creation' do
+      it 'has a new form that can be reached' do
+        visit new_post_path
+        expect(page.status_code).to eq(200)
+      end
+      it 'can be created from new form' do
+        visit new_post_path
+
+        fill_in 'post[category]', with: "rails"
+        fill_in 'post[description]', with: "How to install ruby gem."
+        fill_in 'post[code]', with: "bundle install"
+
+        click_on "Save"
+
+        expect(page).to have_content("How to install ruby gem.")
+      end
 
     end
+
   end
 end
