cdc3a4e7c602bc88072d2c8ccc92e3a0ca744f84
Peter <ruecktenwald@gmail.com>
2019-05-10 19:22:22 -0400

    refactor postcontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,59 +1,61 @@
 class PostsController < ApplicationController
 	before_action :set_post, only: [:show,:edit,:update,:destroy]
+  
 
-
-	def index
-		Post::CATEGORIES.each do |category|
-			case params[:scope]
-				when "#{category}"
-					@posts = Post.where(category: category)
-			end
-		end
-	end
-
-	def new
-		@post = Post.new
-	end
-
-	def create
-		@post = Post.new(post_params)
-		@post.user_id = current_user.id
-
-		if @post.save 
-			redirect_to @post, notice: "You successfully created a new post!"
-		else
-			render :new
-		end
-	end
-
-	def edit
-	end
-
-	def update
-		if @post.update(post_params)
-			redirect_to @post, notice: "You successfully updated your post!"
-		else
-			render :edit
-		end
-	end
-
-	def show	
-	end
-
-	def destroy
-		@post.delete
-		redirect_to posts_path, notice: 'Your post was deleted successfully'
-	end
+  def index 
+   if params[:category]
+     @posts = Post.where(category: params[:category])
+   else 
+    @posts = Post.all
+  end
 
 end
+
+def new
+  @post = Post.new 
+end
+
+def create
+  @post = Post.new(post_params)
+  @post.user_id = current_user.id
+
+  if @post.save 
+   redirect_to @post, notice: "You successfully created a new post!"
+ else
+   render :new
+ end
+end
+
+def edit
+end
+
+def update
+  if @post.update(post_params)
+   redirect_to @post, notice: "You successfully updated your post!"
+ else
+   render :edit
+ end
+end
+
+def show	
+end
+
+def destroy
+  @post.delete
+  redirect_to posts_path, notice: 'Your post was deleted successfully'
+end
+
+
 
 
 private
 
 def post_params
-	params.require(:post).permit(:category,:description,:code,:user_id)
+ params.require(:post).permit(:category,:description,:code,:user_id)
 end
 
 def set_post
-	@post = Post.find(params[:id])
+ @post = Post.find(params[:id])
+end
+
 end
