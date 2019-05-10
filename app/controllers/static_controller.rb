a23784c3e3ec2f71ad541513bb4880df38ab81cb
Peter <ruecktenwald@gmail.com>
2019-05-10 19:28:17 -0400

    did nothing

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,4 @@
 class StaticController < ApplicationController
-	def homepage
+	def homepage   
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,23 +1,19 @@
 class Post < ApplicationRecord  
-	belongs_to :user
-	validates_presence_of :category, :description, :code
-
-	CATEGORIES = [
-		"Rails",
-		"Javascript",
-		"HTML",
-		"Ruby", 
-		"Angular",
-		"Bootstrap",
-		"Heroku",
-		"Git",
-		"Typescript",
-		"React"
-	]
+  belongs_to :user
+  validates_presence_of :category, :description, :code
 
 
-	
+  CATEGORIES = [
+    "Rails",
+    "Javascript",
+    "HTML",
+    "Ruby", 
+    "Angular",
+    "Bootstrap",
+    "Heroku",
+    "Git",
+    "Typescript",
+    "React"
+  ]
 
-
-
-end
+  end
