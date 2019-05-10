83147131e34bd8bc00925310ace9d1b2e01f565a
Peter <ruecktenwald@gmail.com>
2019-05-10 19:23:22 -0400

    create before_action set_categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
+  before_action :set_categories
+    
+
+    
+private
+  def set_categories
+  @categories = Post::CATEGORIES.first(4)
+  end
 end
