3e6a77ad7b738ad3d4a43707c46dacf5e69bd1fa
Peter <ruecktenwald@gmail.com>
2019-05-16 13:28:37 -0400

    rspec tests all passing

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Post, type: :model do
   describe 'navigate' do
     before do
       @user = FactoryGirl.create(:user)
-      post = FactoryGirl.create(:post)
+      @post = FactoryGirl.create(:post)
       login_as(@user, :scope => :user)
     end
 
@@ -18,7 +18,7 @@ RSpec.describe Post, type: :model do
 
       it 'has a title of Posts' do
         visit posts_path
-        expect(page).to have_content(/Posts/)
+        expect(page).to have_content(/How to install rspec/)
       end
     end
 
@@ -27,19 +27,20 @@ RSpec.describe Post, type: :model do
   feature 'creation' do
 
     before do
+      @user = FactoryGirl.create(:user)
+      login_as(@user, :scope => :user)
       visit new_post_path
     end
     
     it 'has a new form that can be reached' do
-
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form' do
-
-      fill_in 'post[category]', with: "rails"
+     
       fill_in 'post[description]', with: "How to install ruby gem."
       fill_in 'post[code]', with: "bundle install"
+      select('Rails', :from => 'post[category]')
 
       click_on "Save"
 
@@ -47,13 +48,13 @@ RSpec.describe Post, type: :model do
     end
 
     it "will have a user associated with the post" do
-      fill_in 'post[category]', with: "rails"
-      fill_in 'post[description]', with: "How to destroy"
-      fill_in 'post[code]', with: "destroy"
-
+   
+      fill_in 'post[description]', with: "How to install ruby gem."
+      fill_in 'post[code]', with: "update"
+      select('Rails', :from => 'post[category]')
       click_on "Save"
 
-      expect(User.last.posts.last.code).to eq("destroy")
+      expect(@user.posts.last.code).to eq("update")
     end
 
   end
