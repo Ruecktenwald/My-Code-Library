f812f2158cee1ec0038ada5e390bd88e5b372ff6
Peter <ruecktenwald@gmail.com>
2019-05-17 17:31:25 -0400

    delete devise helpers

--- app/helpers/devise_helper.rb
+++
@@ -1,8 +0,0 @@
-module DeviseHelper
-  def devise_error_messages!
-    if resource.errors.full_messages.any?
-        flash.now[:error] = resource.errors.full_messages
-    end
-    return ''
-  end
-end
