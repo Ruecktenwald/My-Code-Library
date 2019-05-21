04b644a2442259cb175a1d6e77cbdbfc76bec48c
Peter <ruecktenwald@gmail.com>
2019-05-21 11:20:04 -0400

    add dependant destroy to devise user model for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable     
   
