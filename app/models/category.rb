ab6cd11c6af6eca35af7268b99e5b88e91a6f883
Peter <ruecktenwald@gmail.com>
2019-05-24 21:00:38 -0400

    remove unused scopes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,5 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :posts, dependent: :destroy
   validates_presence_of :name
-  scope :category_by, ->(user) { where(user_id: user.id).order(name: :asc) }
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :category
-  scope :posts_by, ->(user) { where(user_id: user.id).order(description: :asc) }
   validates_presence_of :description, :code
     
 end
