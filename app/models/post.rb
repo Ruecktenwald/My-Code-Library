7489614094120932d386c8623bdd44b56ffb1a5b
Peter <ruecktenwald@gmail.com>
2019-05-24 20:50:16 -0400

    remove post.user_id from create in post controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :category
-  belongs_to :user
   scope :posts_by, ->(user) { where(user_id: user.id).order(description: :asc) }
   validates_presence_of :description, :code
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190522181043) do
+ActiveRecord::Schema.define(version: 20190524235548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,8 @@ ActiveRecord::Schema.define(version: 20190522181043) do
     t.string   "code"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id"
     t.integer  "category_id"
     t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
-    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,5 +49,4 @@ ActiveRecord::Schema.define(version: 20190522181043) do
 
   add_foreign_key "categories", "users"
   add_foreign_key "posts", "categories"
-  add_foreign_key "posts", "users"
 end
