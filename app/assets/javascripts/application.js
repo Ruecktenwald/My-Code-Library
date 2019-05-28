b9d7ed3841f73986ef1e00064d9471bd75a60af2
Peter <ruecktenwald@gmail.com>
2019-05-28 12:38:38 -0400

    add jquery for nav display -changes on size and load for mobile and web specs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
@@ -21,18 +8,54 @@ window.setTimeout(function() {
   $(".alert").addClass("in");
 }, 600);
 
-$(document).ready(function() {
-  
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      console.log($(window).scrollTop())
+$(document).ready(function() { 
+  $(window).scroll(function () { 
+    console.log($(window).scrollTop())
     if ($(window).scrollTop() > 56) {
       $('#nav_bar').addClass('navbar-fixed');
     }
     if ($(window).scrollTop() < 57) {
       $('#nav_bar').removeClass('navbar-fixed');
     }
+    if ($(window).scrollTop() > 56) {
+      $('#nav_bar_web').addClass('navbar-fixed-web');
+    }
+    if ($(window).scrollTop() < 57) {
+      $('#nav_bar_web').removeClass('navbar-fixed-web');
+    }
   });
 });
+
+$(window).resize(function() {
+  if ($(window).width() < 755) {
+    $("#web").hide();
+    $("#mob").show();
+  }
+  else{
+    $("#web").show();
+    $("#mob").hide();
+  }
+});
+
+$(window).load(function() {
+  if ($(window).width() < 755) {
+    $("#web").hide();
+    $("#mob").show();
+  }
+  else{
+    $("#web").show();
+    $("#mob").hide();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
