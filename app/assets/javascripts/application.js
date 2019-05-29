5022d61a6f26dfe8b78b188e69c8c21533809683
Peter <ruecktenwald@gmail.com>
2019-05-29 17:08:22 -0400

    adjust sticky nav timing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,16 +11,16 @@ window.setTimeout(function() {
 $(document).ready(function() { 
   $(window).scroll(function () { 
     console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 56) {
+    if ($(window).scrollTop() > 86) {
       $('#nav_bar').addClass('navbar-fixed');
     }
-    if ($(window).scrollTop() < 57) {
+    if ($(window).scrollTop() < 87) {
       $('#nav_bar').removeClass('navbar-fixed');
     }
-    if ($(window).scrollTop() > 56) {
+    if ($(window).scrollTop() > 86) {
       $('#nav_bar_web').addClass('navbar-fixed-web');
     }
-    if ($(window).scrollTop() < 57) {
+    if ($(window).scrollTop() < 87) {
       $('#nav_bar_web').removeClass('navbar-fixed-web');
     }
   });
