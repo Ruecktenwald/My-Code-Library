5b81f03d416f453569b38593a7b72c21f68eca5e
Peter <ruecktenwald@gmail.com>
2019-06-02 17:52:24 -0400

    add most recent link variable

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
     resources :users
     resources :categories
     resources :posts
-
     root to: "users#index"
   end
-
-  get 'posts/recent' => 'posts#recent', :as => 'most_recent'
-  get 'search' => 'posts#search', :as => 'search_page'
-  get 'category/posts/:category' => 'posts#index', as: 'category_posts'
-  get 'categories/:top-four' => 'categories#top_four', as: 'top-four'
-  resources :categories
-  resources :posts
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   get 'users/profile/:name' => 'users#show', as: 'users_profile'
   root to: 'static#homepage'
+
+  get 'categories/:top-four' => 'categories#top_four', as: 'top-four'
+  resources :categories
+
+  get 'search' => 'posts#search', :as => 'search_page'
+  get 'category/:category' => 'posts#index', as: 'category_posts'
+  get 'most-recent-posts/:recent' => 'posts#recent', :as => 'most_recent'
+  resources :posts  
 end
