9477c7e69878738e1a98c164b9312b7d6dd5fe0a
Peter <ruecktenwald@gmail.com>
2019-06-13 19:16:13 -0400

    update action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,17 +24,19 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { :host => 'https://nameless-journey-14681.herokuapp.com'}
-  
-  ActionMailer::Base.delivery_method = :smtp
- 
+
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SG_USERNAME"],
-  :password => ENV["SG_PASSWORD"],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+    :user_name => ENV["SG_USERNAME"],
+    :password => ENV["SG_PASSWORD"],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
