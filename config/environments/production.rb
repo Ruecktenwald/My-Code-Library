4bb4529094397179ca22c656ed29c70142670b48
Peter <ruecktenwald@gmail.com>
2019-06-13 18:09:12 -0400

    change port number

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: "smtp.sendgrid.net",
-    port: 587,
+    port: 25,
     domain: "heroku.com",
     user_name: ENV["SG_USERNAME"],
     password: ENV["SG_PASSWORD"],
