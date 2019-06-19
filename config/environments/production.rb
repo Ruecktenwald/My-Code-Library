bd226c5a0620ba5435cada97c9206b81c6057746
Peter <ruecktenwald@gmail.com>
2019-06-19 19:40:31 -0400

    add gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,20 +25,20 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.default_url_options = { :host => 'nameless-journey-14681.herokuapp.com'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = { from: "MCLcheatsheets@gmail.com" }
-  
+
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"],
-    openssl_verify_mode: "none"
-  }
+   address: "smtp.gmail.com",
+   port: 587,
+   authentication: "plain",
+   enable_starttls_auto: true,
+   user_name: ENV["GMAIL_USERNAME"],
+   password: ENV["GMAIL_PASSWORD"],
+   openssl_verify_mode: "none"
+ }
 end
