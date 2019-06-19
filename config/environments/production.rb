93804fe6aec719fd3ac91dba7ac31eab892d31a9
Peter <ruecktenwald@gmail.com>
2019-06-19 11:08:57 -0400

    change prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "https://www.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://nameless-journey-14681.herokuapp.com" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
