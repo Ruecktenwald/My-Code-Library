196a8ed7eef70365e9e67ab5d5149ac80819fa39
Peter <ruecktenwald@gmail.com>
2019-06-20 11:45:38 -0400

    update prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,18 +26,18 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: 'nameless-journey-14681.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   host = 'nameless-journey-14681.herokuapp.com'
   protocol = 'https'
   config.action_mailer.default_url_options = { host: host, protocol: protocol}
 
-  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-   :address        => 'smtp.sendgrid.net',
-   :port           => '587',
-   :authentication => :plain,
-   :user_name      => ENV['SENDGRID_USERNAME'],
-   :password       => ENV['SENDGRID_PASSWORD'],
-   :domain         => 'heroku.com',
-   :enable_starttls_auto => true
- }
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+     :domain         => 'heroku.com',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
