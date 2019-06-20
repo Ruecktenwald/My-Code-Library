945be3e392fa4e27a6f5c6077415bddeee134589
Peter <ruecktenwald@gmail.com>
2019-06-20 09:42:51 -0400

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,22 +23,20 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
-
-
-  config.action_mailer.default_url_options = { :host => 'nameless-journey-14681.herokuapp.com'}
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: "MCLcheatsheets@gmail.com" }
-
-  config.action_mailer.smtp_settings = {
-   address: "smtp.gmail.com",
-   port: 587,
-   authentication: "plain",
-   enable_starttls_auto: true,
-   user_name: ENV["GMAIL_USERNAME"],
-   password: ENV["GMAIL_PASSWORD"],
-   openssl_verify_mode: "none"
+  
+config.action_mailer.default_url_options = { host: 'nameless-journey-14681.herokuapp.com' }
+ config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.delivery_method = :smtp
+host = 'nameless-journey-14681.herokuapp.com'
+ protocol = 'https'
+ config.action_mailer.default_url_options = { host: host, protocol: protocol}
+ ActionMailer::Base.smtp_settings = {
+   :address        => 'smtp.sendgrid.net',
+   :port           => '587',
+   :authentication => :plain,
+   :user_name      => ENV['SENDGRID_USERNAME'],
+   :password       => ENV['SENDGRID_PASSWORD'],
+   :domain         => 'heroku.com',
+   :enable_starttls_auto => true
  }
 end
--- send_grid_email.rb
+++ send_grid_email.rb
@@ -12,3 +12,26 @@ response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
 puts response.headers
+
+
+
+
+harry
+
+
+
+config.action_mailer.default_url_options = { host: 'nameless-journey-14681.herokuapp.com' }
+ config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.delivery_method = :smtp
+host = 'nameless-journey-14681.herokuapp.com'
+ protocol = 'https'
+ config.action_mailer.default_url_options = { host: host, protocol: protocol}
+ ActionMailer::Base.smtp_settings = {
+   :address        => 'smtp.sendgrid.net',
+   :port           => '587',
+   :authentication => :plain,
+   :user_name      => ENV['SENDGRID_USERNAME'],
+   :password       => ENV['SENDGRID_PASSWORD'],
+   :domain         => 'heroku.com',
+   :enable_starttls_auto => true
+ }
