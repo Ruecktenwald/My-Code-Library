1a70dd3ccf216da0a1064a5bcfa69469685174f7
Peter <ruecktenwald@gmail.com>
2019-06-20 11:42:06 -0400

    no change

--- send_grid_email.rb
+++ send_grid_email.rb
@@ -14,24 +14,3 @@ puts response.body
 puts response.headers
 
 
-
-
-harry
-
-
-
-config.action_mailer.default_url_options = { host: 'nameless-journey-14681.herokuapp.com' }
- config.action_mailer.raise_delivery_errors = true
- config.action_mailer.delivery_method = :smtp
-host = 'nameless-journey-14681.herokuapp.com'
- protocol = 'https'
- config.action_mailer.default_url_options = { host: host, protocol: protocol}
- ActionMailer::Base.smtp_settings = {
-   :address        => 'smtp.sendgrid.net',
-   :port           => '587',
-   :authentication => :plain,
-   :user_name      => ENV['SENDGRID_USERNAME'],
-   :password       => ENV['SENDGRID_PASSWORD'],
-   :domain         => 'heroku.com',
-   :enable_starttls_auto => true
- }
