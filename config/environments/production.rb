31103e4912daf52f5bb9d9a7310b16f9ee681b33
Peter <ruecktenwald@gmail.com>
2022-08-13 19:38:56 -0400

    config edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = false
   config.log_level = :debug
   config.log_tags = [ :request_id ]
