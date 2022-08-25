700711db74c8936531baaa5ad5abc5d7f4f0571d
Peter <ruecktenwald@gmail.com>
2022-08-25 10:33:21 -0400

    edit app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    notice: "You are not authorized to perform this action."
+    flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
   end
 
